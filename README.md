7ac7db03549c1c97bc1834b468c88383acb073c0
Andreas Streichardt <andreas.streichardt@gmail.com>
2017-02-28 01:26:03 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,28 @@
+# RPck compressor
+
+WIP implementation of an RPck compressor.
+
+## RPck
+
+RPck is a very old and very exotic file compression format used on the Amiga
+(and even there it is super exotic). Decompression tools are available. However
+compression tools are (to my knowledge) non existing.
+
+This is a reimplementation of a compressor derived from the ASM decompression code here:
+
+http://wt.exotica.org.uk/files/others/xfd_RPck.lzx
+
+RPck is a VERY simple format and won't really bring down the size like modern tools would.
+
+If you genuinely need this project and we meet in real life somewhen I owe you a beer.
+
+## Implementation
+
+This is done in crappy rust. I like rust but I am bad at it. Very bad.
+
+After running you can take `test.txt.rpck` and run `xfddecrunch test.txt.rpck` in the
+shell of your Amiga. After that the 
+
+## Tests
+
+none 8]
---
+++ src/main.rs
@@ -0,0 +1,180 @@
+extern crate byteorder;
+
+use std::error::Error;
+use std::fs::{self, OpenOptions, File};
+use std::io::{Result, Read};
+use std::path::Path;
+use byteorder::{WriteBytesExt, BigEndian};
+
+struct Header {
+    magic: [u8; 4],
+    orig_length: u32, // for some reasons the decrucnher asm checks if the first byte is a 0
+                      // maybe it is a real u24?
+    unknown: u32, // probably a checksum... again...checking explicitly for 0 in the asm :S
+}
+
+enum Chunk {
+    Unique {
+        data: Vec<u8>,
+    },
+    Repeated {
+        byte: u8,
+        num: u8,
+    },
+}
+
+fn write_chunk<T: WriteBytesExt>(chunk: Chunk, mut target: T) -> Result<()> {
+    match chunk {
+        Chunk::Unique { data } => {
+            // argh...negating without the compiler being a dingens
+            try!(target.write_i8((data.len() as i32 * -1) as i8));
+            for c in data.iter() {
+                try!(target.write_u8(*c));
+            }
+        },
+        Chunk::Repeated { byte, num } => {
+            try!(target.write_u8(num - 1));
+            try!(target.write_u8(byte));
+        }
+    }
+    Ok(())
+}
+
+fn write_header<T: WriteBytesExt>(length: u64, mut target: T) -> Result<usize> {
+    let header = Header { magic: *b"RPck", orig_length: length as u32, unknown: 0};
+    for c in header.magic.iter() {
+        try!(target.write_u8(*c));
+    }
+    
+    try!(target.write_u32::<BigEndian>(header.orig_length));
+    try!(target.write_u32::<BigEndian>(header.unknown));
+    Ok(1)
+}
+
+fn rpck<T: Read>(rfile: T, size: u64, wfile: File) -> Result<usize> {
+    try!(write_header(size, &wfile));
+
+    let mut current: Option<Chunk> = None;
+
+    for byte in rfile.bytes() {
+        // this is all looking super fuzzy :S n00bish code is n00bish :D
+        let b = byte.unwrap();
+        current = match current {
+            None => Some(Chunk::Unique { data: [b].to_vec()}),
+            Some(current) => {
+                match current {
+                    // pyramid, pyramid!
+                    Chunk::Unique { mut data } => {
+                        if b == *data.last().unwrap() {
+                            // if it is unique (data[0] != data[1]) it must be at least 2 bytes long :S
+                            // (that's what I found out from example files)
+                            match data.len() {
+                                1 => {
+                                    Some(Chunk::Repeated {
+                                        byte: b,
+                                        num: 2,
+                                    })
+                                },
+                                2 => {
+                                    data.push(b);
+                                    Some(Chunk::Unique {
+                                        data: data,
+                                    })
+                                },
+                                127 => {
+                                    try!(write_chunk(Chunk::Unique {
+                                        data: data,
+                                    }, &wfile));
+                                    Some(Chunk::Unique { data: [b].to_vec()})
+                                },
+                                _ => {
+                                    let mut num = 1;
+                                    while data.len() != 2 && *data.last().unwrap() == b {
+                                        num += 1;
+                                        data.pop();
+                                    }
+                                    if data.len() > 0 {
+                                        try!(write_chunk(Chunk::Unique {
+                                            data: data,
+                                        }, &wfile));
+                                    }
+                                    Some(Chunk::Repeated {
+                                        byte: b,
+                                        num: num,
+                                    })
+                                }
+                            }
+                        } else {
+                            data.push(b);
+                            Some(Chunk::Unique {
+                                data: data,
+                            })
+                        }
+                    },
+                    Chunk::Repeated { byte, mut num } => {
+                        if b == byte {
+                            num += 1;
+                            let result = Chunk::Repeated {
+                                byte: b,
+                                num: num,
+                            };
+                            if num < 127 {
+                                Some(result)
+                            } else {
+                                try!(write_chunk(result, &wfile));
+                                None
+                            }
+                        } else {
+                            try!(write_chunk(current, &wfile));
+                            Some(Chunk::Unique { data: [b].to_vec()})
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if current.is_some() {
+        try!(write_chunk(current.unwrap(), wfile));
+    }
+    Ok(1)
+}
+
+fn main() {
+    let rfilename = "test.txt";
+    let wfilename = "test.txt.rpck";
+    // Create a path to the desired file
+    let rpath = Path::new(rfilename);
+    let wpath = Path::new(wfilename);
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let rfile = match File::open(&rpath) {
+        // The `description` method of `io::Error` returns a string that
+        // describes the error
+        Err(why) => panic!("couldn't open {}: {}", rfilename,
+                                                   why.description()),
+        Ok(file) => file,
+    };
+
+    let metadata = match fs::metadata(rfilename) {
+        Err(why) => panic!("couldn't read metadata {}: {}", rfilename,
+                                            why.description()),
+        Ok(metadata) => metadata,
+    };
+
+    let wfile = match OpenOptions::new()
+                             .create(true)
+                             .truncate(true)
+                             .write(true)
+                             .open(&wpath) {
+        // The `description` method of `io::Error` returns a string that
+        // describes the error
+        Err(why) => panic!("couldn't open {}: {}", wfilename,
+                                                   why.description()),
+        Ok(file) => file,
+    };
+
+    match rpck(rfile, metadata.len(), wfile) {
+        Err(why) => panic!("couldn't write: {}", why.description()),
+        Ok(_) => ()
+    }
+}
